6dcd36c17ec30260953c743158362540c888f6bf
stepushkin33 <56911991+stepushkin33@users.noreply.github.com>
2022-10-14 17:08:21 +0300

    initial commit

---
+++ test/form.spec.js
@@ -0,0 +1,65 @@
+// type "npm test" on terminal to start spammer
+
+import { faker } from "@faker-js/faker";
+import puppeteer from "puppeteer";
+
+// registration form's URL
+const APP = "https://b2b-fair.online/profiles-add/";
+
+// browser settings
+let page;
+let browser;
+const width = 1920;
+const height = 1080;
+
+//init spammer function
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 0,
+    args: [`--window-size=${width},${height}`],
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+});
+afterAll(() => {
+  browser.close();
+});
+
+//spam script
+describe("Registration form", () => {
+  test("spam registration form", async () => {
+    for (let i = 0; i <= 100; i++) {
+      const lead = {
+        company: faker.company.name(),
+        name: faker.name.firstName(),
+        secondName: faker.name.lastName(),
+        phone: faker.phone.number(`89#########`),
+        email: faker.internet.email(),
+        password: "1234567890",
+      };
+
+      await page.goto(APP);
+      await page.waitForSelector(".clearfix");
+      await page.click("#elm_8", { clickCount: 3 });
+      await page.type("#elm_8", lead.company);
+      await page.click("#elm_6", { clickCount: 3 });
+      await page.type("#elm_6", lead.name);
+      await page.click("#elm_7", { clickCount: 3 });
+      await page.type("#elm_7", lead.secondName);
+      await page.click("#elm_9", { clickCount: 3 });
+      await page.type("#elm_9", lead.phone);
+      await page.click("#email", { clickCount: 3 });
+      await page.type("#email", lead.email);
+      await page.click("#password1", { clickCount: 3 });
+      await page.type("#password1", lead.password);
+      await page.click("#password2", { clickCount: 3 });
+      await page.type("#password2", lead.password);
+      await page.click("#gdpr_agreements_user_registration");
+      await page.click("#elm_8");
+      await page.click("#gdpr_agreements_newsletters_subscribe");
+      page.keyboard.press("Enter");
+      await page.waitForSelector(".alert-warning");
+    }
+  }, 1600000);
+});
